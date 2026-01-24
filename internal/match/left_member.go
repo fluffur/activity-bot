794d3d244a30d62c18313f37e73891e70ba5eb8b
fluffur <flurffur@gmail.com>
2026-01-24 23:10:24 +0700

    ignore bots kick

--- internal/match/left_member.go
+++ internal/match/left_member.go
@@ -3,5 +3,5 @@ package match
 import "github.com/go-telegram/bot/models"
 
 func LeftMember(update *models.Update) bool {
-	return update.Message != nil && update.Message.LeftChatMember != nil
+	return update.Message != nil && update.Message.LeftChatMember != nil && !update.Message.LeftChatMember.IsBot
 }
