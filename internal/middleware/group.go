918fbbc73136c7c9a4bc6122311b86b7512d5beb
fluffur <flurffur@gmail.com>
2026-01-24 22:45:00 +0700

    fix nil pointer

--- internal/middleware/group.go
+++ internal/middleware/group.go
@@ -10,27 +10,15 @@ import (
 func OnlyGroups(next bot.HandlerFunc) bot.HandlerFunc {
 	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
 		m := update.Message
-		if m == nil {
+		if m == nil && update.CallbackQuery != nil {
 			m = update.CallbackQuery.Message.Message
+		} else {
+			next(ctx, b, update)
+			return
 		}
 		if m == nil || m.Chat.Type == "private" {
 			return
 		}
 		next(ctx, b, update)
 	}
-}
-
-func OnlyGroupsMatch(update *models.Update) bool {
-	var m *models.Message
-
-	if update.Message != nil {
-		m = update.Message
-	} else if update.CallbackQuery != nil {
-		m = update.CallbackQuery.Message.Message
-	} else {
-		return false
-	}
-
-	return m != nil &&
-		m.Chat.Type != "private"
 }
