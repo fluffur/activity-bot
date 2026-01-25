6aa1bb6733982668f447fca1f35ac960b71d6620
fluffur <flurffur@gmail.com>
2026-01-25 22:16:52 +0700

    cleanup

--- internal/helpers/permissions.go
+++ internal/helpers/permissions.go
@@ -1,37 +1,14 @@
 package helpers
 
 import (
-	"context"
 	"log"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/go-telegram/bot"
 )
 
 type AdminService interface {
 	IsAdmin(chatID, userID int64) (bool, error)
-}
-
-func CheckOwnerOrAdmin(ctx context.Context, b *bot.Bot, adminService AdminService, chatID, userID int64) bool {
-	member, err := b.GetChatMember(ctx, &bot.GetChatMemberParams{
-		ChatID: chatID,
-		UserID: userID,
-	})
-	if err != nil {
-		log.Printf("Failed to get chat member for owner check: %v", err)
-		return false
-	}
-	if member.Owner != nil {
-		return true
-	}
-
-	isAdmin, err := adminService.IsAdmin(chatID, userID)
-	if err != nil {
-		log.Printf("Failed to check db admin: %v", err)
-		return false
-	}
-	return isAdmin
 }
 
 func IsSenderAdmin(b *gotgbot.Bot, ctx *ext.Context, adminService AdminService) bool {
