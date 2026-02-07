0ad74be56b4c6d47a89b3f7eda54eecf3890c4d1
fluffur <flurffur@gmail.com>
2026-02-07 22:49:18 +0700

    migration fixes

--- migrations/00018_create_moderation_actions_table.sql
+++ migrations/00018_create_moderation_actions_table.sql
@@ -25,19 +25,8 @@ ALTER TABLE chats
 -- +goose StatementBegin
 ALTER TABLE chats
     DROP COLUMN max_warns;
-DROP TABLE moderation_actions;
-DROP TYPE moderation_type;
 
-CREATE TABLE bans
-(
-    id         SERIAL PRIMARY KEY,
-    chat_id    BIGINT    NOT NULL,
-    user_id    BIGINT    NOT NULL,
-    reason     TEXT,
-    banned_by  BIGINT    NOT NULL,
-    banned_at  TIMESTAMP NOT NULL DEFAULT NOW(),
-    until_date TIMESTAMP,
-    FOREIGN KEY (chat_id) REFERENCES chats (id) ON DELETE CASCADE
-);
-CREATE INDEX idx_bans_chat_user ON bans (chat_id, user_id);
+DROP TABLE moderation_actions;
+
+DROP TYPE moderation_type;
 -- +goose StatementEnd
