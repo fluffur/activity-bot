be651773bf8dbbd2901e83fa18043f8184d619af
fluffur <flurffur@gmail.com>
2026-02-08 00:08:08 +0700

    fix moderation actions table

--- migrations/00018_create_moderation_actions_table.sql
+++ migrations/00018_create_moderation_actions_table.sql
@@ -4,18 +4,21 @@ CREATE TYPE moderation_type AS ENUM ('kick', 'mute', 'ban', 'warn');
 
 CREATE TABLE moderation_actions
 (
-    id         SERIAL PRIMARY KEY,
-    type       moderation_type NOT NULL,
-    chat_id    BIGINT          NOT NULL REFERENCES chats (id) ON DELETE CASCADE,
-    user_id    BIGINT          NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    mod_id     BIGINT          NOT NULL,
-    reason     TEXT,
-    created_at TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
-    revoked_at TIMESTAMPTZ,
-    until_date TIMESTAMPTZ
+    id           BIGSERIAL PRIMARY KEY,
+    type         moderation_type NOT NULL,
+    chat_id      BIGINT          NOT NULL REFERENCES chats (id) ON DELETE CASCADE,
+    user_id      BIGINT          NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    moderator_id BIGINT          NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    reason       TEXT,
+    created_at   TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+    revoked_at   TIMESTAMPTZ,
+    expires_at   TIMESTAMPTZ,
+    UNIQUE (chat_id, user_id, type, created_at)
 );
 
-CREATE INDEX idx_moderation_recent ON moderation_actions (chat_id, user_id, type, created_at);
+CREATE INDEX idx_moderation_active
+    ON moderation_actions (chat_id, user_id, type)
+    WHERE revoked_at IS NULL;
 
 ALTER TABLE chats
     ADD COLUMN max_warns INT NOT NULL DEFAULT 3;
