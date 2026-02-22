2fed9d1b527041877400fd2fbcb7d922e140639c
fluffur <flurffur@gmail.com>
2026-02-22 17:07:02 +0700

    fix checks

--- internal/cmd/command.go
+++ internal/cmd/command.go
@@ -183,7 +183,11 @@ func (c *Command) parseArgs(b *gotgbot.Bot, ctx *ext.Context, cctx context.Conte
 			}
 		}
 	}
-	if msg.ReplyToMessage != nil && msg.ReplyToMessage.From != nil && !msg.ReplyToMessage.From.IsBot && !msg.ReplyToMessage.IsAutomaticForward {
+	if msg.ReplyToMessage != nil &&
+		msg.ReplyToMessage.ForumTopicCreated == nil &&
+		msg.ReplyToMessage.From != nil &&
+		!msg.ReplyToMessage.From.IsBot &&
+		!msg.ReplyToMessage.IsAutomaticForward {
 		log.Printf("reply message %#+v", msg.ReplyToMessage)
 		u, err := c.ensureUser(cctx, msg.ReplyToMessage.From)
 		if err == nil {
