1021777b88c557d3d2f6dad9ce048c5a65f4eea8
fluffur <flurffur@gmail.com>
2026-02-22 17:01:58 +0700

    add logs

--- internal/cmd/command.go
+++ internal/cmd/command.go
@@ -184,6 +184,7 @@ func (c *Command) parseArgs(b *gotgbot.Bot, ctx *ext.Context, cctx context.Conte
 		}
 	}
 	if msg.ReplyToMessage != nil && msg.ReplyToMessage.From != nil && !msg.ReplyToMessage.From.IsBot && !msg.ReplyToMessage.IsAutomaticForward {
+		log.Printf("reply message %#+v", msg.ReplyToMessage)
 		u, err := c.ensureUser(cctx, msg.ReplyToMessage.From)
 		if err == nil {
 			log.Println("Reply user", u.FirstName, u.Username, u.ID)
