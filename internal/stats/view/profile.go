83a0c6ceaa2f6a7024f71b4cbae5eafa9ec1c235
fluffur <flurffur@gmail.com>
2026-02-25 18:36:48 +0700

    improve profile format

--- internal/stats/view/profile.go
+++ internal/stats/view/profile.go
@@ -11,62 +11,83 @@ func FormatProfile(m model.MemberStats) string {
 	if m.CustomTitle != nil && *m.CustomTitle != "" {
 		customTitle = *m.CustomTitle
 	}
-	extraText := ""
+
+	name := helpers.LinkWithContent(
+		m.User,
+		fmt.Sprintf("%s (%s)", m.User.FirstName, customTitle),
+	)
+
+	status := helpers.TranslateMemberStatus(m.Status)
+
+	text := fmt.Sprintf(
+		`👤 <b>Информация о:</b> %s
+👑 <b>Статус:</b> %s (с %s)
+	
+📊 <b>Активность</b>
+└ Всего: <b>%d</b>
+└ Сегодня: <b>%d</b>
+└ Неделя: <b>%d</b>
+
+⏰ <b>Динамика</b>
+└ 24ч: <b>%d</b> | 7д: <b>%d</b> | 30д: <b>%d</b>`,
+		name,
+		status,
+		helpers.FormatToHumanDate(m.JoinedAt),
+		m.AllTime,
+		m.DayCount,
+		m.WeekCount,
+		m.DayRollingCount,
+		m.WeekRollingCount,
+		m.MonthRollingCount,
+	)
 
 	if m.RestUntil != nil {
-		extraText = fmt.Sprintf("💤 Рест до %s", helpers.FormatToHumanDate(*m.RestUntil))
-	} else {
+		text += fmt.Sprintf(
+			"\n\n💤 <b>Рест до:</b> %s",
+			helpers.FormatToHumanDate(*m.RestUntil),
+		)
+	} else if m.NormWarn > 0 || m.NormBan > 0 {
+
+		normEmoji := getNormEmoji(
+			m.WeekCount,
+			m.NormBan,
+			m.NormWarn,
+		)
+
+		text += fmt.Sprintf("\n\n%s <b>Норма (неделя)</b>\n", normEmoji)
+
 		if m.NormWarn > 0 {
-			warnEmoji := "❌"
-			if m.WeekCount >= m.NormWarn {
-				warnEmoji = "✅"
-			}
-			extraText += fmt.Sprintf(
-				"%s <b>Норма (варн)</b>: %d/%d за эту неделю",
-				warnEmoji,
+			text += fmt.Sprintf(
+				"└ Варн: <b>%d</b> / %d\n",
 				m.WeekCount,
 				m.NormWarn,
 			)
-
 		}
 
 		if m.NormBan > 0 {
-			banEmoji := "❌"
-			if m.WeekCount >= m.NormBan {
-				banEmoji = "✅"
-			}
-			extraText += fmt.Sprintf(
-				"\n%s <b>Норма (бан)</b>: %d/%d за эту неделю",
-				banEmoji,
+			text += fmt.Sprintf(
+				"└ Бан: <b>%d</b> / %d",
 				m.WeekCount,
 				m.NormBan,
 			)
 		}
 	}
 
-	return fmt.Sprintf(
-		`<b>📊 Информация о %s</b>
+	return text
+}
 
-🌟 Статус: <b>%s</b> | Присоединился: <b>%s</b>
+func getNormEmoji(weekCount, normBan, normWarn int32) string {
+	if normBan == 0 && normWarn == 0 {
+		return "⚪"
+	}
 
-<b>📅 Активность</b>: сегодня <b>%d</b> | неделя <b>%d</b> | месяц <b>%d</b>
+	if weekCount < normBan {
+		return "❌"
+	}
 
-<b>🔄 Активность в последние</b>: 24ч <b>%d</b> | 7д <b>%d</b> | 30д <b>%d</b>
+	if weekCount < normWarn {
+		return "⚠️"
+	}
 
-📝 <b>Всего сообщений:</b> %d
-
-%s
-`,
-		helpers.LinkWithContent(m.User, fmt.Sprintf("%s (%s)", m.User.FirstName, customTitle)),
-		helpers.TranslateMemberStatus(m.Status),
-		helpers.FormatToHumanDate(m.JoinedAt),
-		m.DayCount,
-		m.WeekCount,
-		m.MonthCount,
-		m.DayRollingCount,
-		m.WeekRollingCount,
-		m.MonthRollingCount,
-		m.AllTime,
-		extraText,
-	)
+	return "✅"
 }
