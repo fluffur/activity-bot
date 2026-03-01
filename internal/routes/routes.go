5e1a445bd1a3a6f9b625e637bcbc48e799ef68ff
fluffur <flurffur@gmail.com>
2026-03-01 12:19:12 +0700

    Revert "embed html"
    This reverts commit c10c3efb7f405fe5c26235b65a224c2247b333e3.

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	_ "embed"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -12,9 +11,7 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-//go:embed index.html
-var indexHTML string
-var indexTmpl = template.Must(template.New("index").Parse(indexHTML))
+var indexTmpl = template.Must(template.ParseFiles("index.html"))
 
 func Index(webappURL string) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
