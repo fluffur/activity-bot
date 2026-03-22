7a20c84a3fd5880dfa1d729e027a21bfab406441
fluffur <flurffur@gmail.com>
2026-03-22 19:02:38 +0700

    disable post broadcast

--- internal/channel/handler/handler.go
+++ internal/channel/handler/handler.go
@@ -4,10 +4,6 @@ import (
 	"activity-bot/internal/chat"
 	"activity-bot/internal/cmd"
 	"activity-bot/internal/member"
-	"activity-bot/internal/model"
-	"encoding/json"
-	"fmt"
-	"log"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/hibiken/asynq"
@@ -25,33 +21,33 @@ func New(memberService *member.Service, chatService *chat.Service, asyncClient *
 }
 
 func (h *Handler) Post(b *gotgbot.Bot, ctx *cmd.Context) error {
-	log.Println("post", ctx.EffectiveChat.Id, h.channelID)
-	if ctx.EffectiveChat.Id != h.channelID {
-		return nil
-	}
-
-	chats, err := h.chatService.GetChatsWithEnabledBroadcast(ctx.StdContext())
-	if err != nil {
-		return fmt.Errorf("failed to get chats: %w", err)
-	}
-	msg := ctx.EffectiveMessage
-
-	for _, c := range chats {
-
-		payload, _ := json.Marshal(model.BroadcastPayload{
-			ChatID:     c.ID,
-			FromChatID: msg.Chat.Id,
-			MessageID:  msg.MessageId,
-		})
-
-		task := asynq.NewTask("broadcast:post", payload)
-
-		_, err := h.asyncClient.Enqueue(task)
-		if err != nil {
-			return err
-		}
-	}
-
+	//log.Println("post", ctx.EffectiveChat.Id, h.channelID)
+	//if ctx.EffectiveChat.Id != h.channelID {
+	//	return nil
+	//}
+	//
+	//chats, err := h.chatService.GetChatsWithEnabledBroadcast(ctx.StdContext())
+	//if err != nil {
+	//	return fmt.Errorf("failed to get chats: %w", err)
+	//}
+	//msg := ctx.EffectiveMessage
+	//
+	//for _, c := range chats {
+	//
+	//	payload, _ := json.Marshal(model.BroadcastPayload{
+	//		ChatID:     c.ID,
+	//		FromChatID: msg.Chat.Id,
+	//		MessageID:  msg.MessageId,
+	//	})
+	//
+	//	task := asynq.NewTask("broadcast:post", payload)
+	//
+	//	_, err := h.asyncClient.Enqueue(task)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
 	return nil
 }
 
