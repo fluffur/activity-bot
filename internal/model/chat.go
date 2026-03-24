fe786a4f9c83b2e29240f81a9aa6fac7b2913db9
fluffur <flurffur@gmail.com>
2026-03-24 21:40:34 +0700

    fix is status

--- internal/model/chat.go
+++ internal/model/chat.go
@@ -51,7 +51,7 @@ func (cm ChatMember) CanModerate(c ChatMember) bool {
 }
 
 func (cm ChatMember) IsStatus(s Status) bool {
-	return s >= cm.Status
+	return cm.Status >= s
 }
 
 func (cm ChatMember) IsRestActive(now time.Time) bool {
