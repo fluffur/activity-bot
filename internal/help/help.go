d3c1443707a92213744500318f4ea918cbf89817
fluffur <flurffur@gmail.com>
2026-06-20 14:49:23 +0500

    fix: help intl

--- internal/help/help.go
+++ internal/help/help.go
@@ -2,21 +2,26 @@ package help
 
 import (
 	"activity-bot/internal/i18n"
+	"activity-bot/internal/middleware/cctx"
 	"activity-bot/internal/utils/tghtml"
+	"fmt"
 
 	"github.com/gotd/botapi"
 )
 
 func (h *Handler) Help(c *botapi.Context) error {
+	ch, err := cctx.Chat(c.Context)
+	if err != nil {
+		return fmt.Errorf("help chat ctx: %w", err)
+	}
+
 	args := i18n.HelpArgs(
-		tghtml.Bold(
-			tghtml.Link(h.commandsURL, h.translator.TData("ru", i18n.BotCommands, nil)),
-		),
+		tghtml.Bold(tghtml.Link(h.commandsURL, h.translator.T(ch.Language, i18n.BotCommands))),
 		tghtml.UserLink(h.developerUsername),
 	)
 
-	_, err := c.Reply(
-		h.translator.TData("ru", i18n.Help, args),
+	_, err = c.Reply(
+		h.translator.TData(ch.Language, i18n.Help, args),
 		botapi.WithParseMode(botapi.ParseModeHTML),
 	)
 
