35311389b49eaec0099fb4b3b31c44697768b86d
fluffur <flurffur@gmail.com>
2026-07-25 12:44:35 +0500

    fix

--- internal/info/roles.go
+++ internal/info/roles.go
@@ -32,7 +32,7 @@ const rolesTemplate = `˚   𝘇 𐰁   𓆩 🗯 𓆪 ㅤ姿態哦   ૮ > . �
 
 {{range .Categories}}
 {{.Emoji}} {{.Name}}
-<blockquote expandable>{{range .Roles-}}
+<blockquote expandable>{{range .Roles -}}
 {{.Name}} - {{.Status}}{{end}}
 </blockquote>
 {{end}}
