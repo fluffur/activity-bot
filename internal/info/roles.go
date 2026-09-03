c2159c4a0fda21abccf1a1f62ecd6acdd76756e3
cake <fruitcakehomie@gmail.com>
2026-09-04 01:11:09 +0700

    separate by chars

--- internal/info/roles.go
+++ internal/info/roles.go
@@ -29,12 +29,9 @@ type RenderRole struct {
 	Status string
 }
 
-const rolesTemplate = `{{.Header}}
-{{range .Categories -}}
+const rolesTemplate = `{{.Header}}{{range .Categories}}
 {{.Name}}
-<blockquote expandable>{{.Text}}</blockquote>
-
-{{end}}`
+<blockquote expandable>{{.Text}}</blockquote>{{end}}`
 
 var rolesTmpl = template.Must(
 	template.New("roles").Parse(rolesTemplate),
